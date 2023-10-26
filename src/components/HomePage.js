31738099552d4315d2fed8ef64b767f8494fe3f2
k-galvin <110426866+k-galvin@users.noreply.github.com>
2023-10-26 10:43:39 -0700

    Add images, format pages, and update favicon

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,23 @@
 export default function HomePage({ articles, setArticle, setReading, setCurrentPage, imagesLoaded, handleImagesLoad }) {
   return (
     <div className="home">
-      <h1 className="home-title">Welcome to Autumn Agenda!</h1>
-      <h2 className="home-body">Check out some of our most recent articles:</h2>
+      <h1 className="home-title">
+        <span className="home-element-inner">Welcome to Autumn Agenda!</span>
+      </h1>
+      <h2 className="home-body">
+        <span className="home-element-inner">Check out some of our most recent articles:</span>
+      </h2>
       <div className="home-links">
         {!articles
           ? 'No articles'
           : articles.slice(0, 3).map(a => (
-              <p key={a.id} onClick={() => setArticle(a) & setReading(true) & setCurrentPage('all')}>
+              <span
+                className="home-element-inner"
+                key={a.id}
+                onClick={() => setArticle(a) & setReading(true) & setCurrentPage('all')}
+              >
                 {a.title}
-              </p>
+              </span>
             ))}
       </div>
       <img className="home-img falling-leaf" src="/fall-leaf.png" alt="fall leaf" onLoad={handleImagesLoad}></img>
--- src/components/LifestylePage.js
+++ src/components/LifestylePage.js
@@ -4,8 +4,9 @@ export default function LifestylePage({ articles, setArticle, setReading }) {
     <div className="topic-page">
       <Nav articles={articles} setArticle={setArticle} setReading={setReading} />
       <div className="topic-page-content">
-        <h1>Lifestyle</h1>
-        <h2>Select an article</h2>
+        <h1 className="topic-page-title">Lifestyle</h1>
+        <h3 className="topic-page-body">Select an article from the nav bar</h3>
+        <img className="topic-page-img" src="/book.png"></img>
       </div>
     </div>
   )
